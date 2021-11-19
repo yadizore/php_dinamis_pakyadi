f2b9aabee606f37794cf1d246ed5004991acdf4c
yadizore <nduty560@gmail.com>
2021-11-19 08:43:01 +0700

    versi pertama

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <!-- ini adalah pembuka navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Hasqibar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Alamat</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Hobi</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cita - Cita</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- ini adalah penutup navbar -->
+        <!-- Content 1 -->
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div class="card mx-auto" style="width: 18rem;">
+                        <img src="assets/img/qibar.png" class="card-img-top" style="width: 9rem;">
+                        <div class="card-body">
+                            <p class="card-text">Qibar</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-8">
+                    <h1>Selamat datang di website saya</h1>
+                    <p>Perkenalkan saya Muhammad Rizqi Akbar Rabbani biasa dipanggil Qibar , saya memiliki kemampuan untuk menggambar desain grafis dengan konsep yang matang , lalu saya menyukai pemograman tetapi tidak terlalu fokus dalam bidang pemograman , saya suka indomie pake telor sambil nonton 100 mob pyshco, komunikasi saya menggunakan wa dan ig </p>
+                </div>
+            </div>
+        </div>
+        <!-- Content 1 -->
+    </div>
+
+
+    <!-- Link Javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
