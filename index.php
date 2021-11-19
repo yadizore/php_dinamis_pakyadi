2f74dff6b53db5889977fd1fb4d479d1885b3990
yadizore <nduty560@gmail.com>
2021-11-19 08:44:09 +0700

    abis nambahin mantan

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <!-- ini adalah pembuka navbar -->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Hasqibar</a>
+                <a class="navbar-brand" href="#">Adel</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
